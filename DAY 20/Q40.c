fe78e0d165f2449fd19070ed9aa9cea817888146
64m9d9zbbn-cpu <64m9d9zbbn@privaterelay.appleid.com>
2026-09-13 13:52:12 +0530

    Added Day 20 programs

---
+++ DAY 20/Q40.c
@@ -0,0 +1,36 @@
+//Q40 : Write a program to find the 1’s complement of a binary number and print it.
+
+      /*
+      Sample Test Cases:
+      Input 1:
+      1010
+      Output 1:
+      0101
+
+      Input 2:
+      1111
+      Output 2:
+      0000
+
+      */ 
+     #include <stdio.h>
+     int main() {
+      long n, result = 0, place = 1;
+      int digit;
+      printf("Enter a binary number : ");
+      scanf("%ld ", &n);
+      while( n != 0)
+      {
+            digit = n % 10;
+
+            if(digit == 0)
+            digit = 1;
+            else
+            digit = 0;
+            result = result + digit * place;
+            place = place * 10;
+            n = n / 10;
+      }
+      printf("1's complement = %ld", result);
+      return 0;
+}
