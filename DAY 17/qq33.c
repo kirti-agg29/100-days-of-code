b74d68fff28ada53b93d0b38da5959326271c452
64m9d9zbbn-cpu <64m9d9zbbn@privaterelay.appleid.com>
2026-09-13 13:44:46 +0530

    Added DAY 17 programs

---
+++ DAY 17/qq33.c
@@ -0,0 +1,34 @@
+//Q33 : Write a program to check if a number is an Armstrong number.
+
+      /*
+      Sample Test Cases:
+      Input 1:
+      153
+      Output 1:
+      Armstrong
+
+      Input 2:
+      123
+      Output 2:
+      Not Armstrong
+
+      */
+     #include <stdio.h>
+     int main() {
+        int n, sum, original, remainder;
+        printf("Enter n : ");
+        scanf("%d", &n);
+        original = n;
+        for(; n !=0; n = n / 10)
+         {
+       remainder = n % 10;
+        sum = sum + remainder * remainder * remainder;
+         }
+         if(sum == original)
+         
+            printf("Armstrong number");
+            else
+            printf("Not armstrong number");
+            return 0;
+         }
+     
